e1648195802efb4aa4a7b146ab38c26cf5ebebaf
LUCA ERBI s319024 <s319024@studenti.polito.it>
2023-09-17 17:38:24 +0200

    Philosphes

--- Philosophes/src/Philophe.java
+++ Philosophes/src/Philophe.java
@@ -1,8 +1,21 @@
 
 public class Philophe extends Thread{
     private String name = null;
+    private Table table = null;
+    private int pos = -1;
+    private boolean hasEaten = false;
 
-    private Philophe(String name){
+    public Philophe(String name, Table table, int pos){
+        this.table = table;
         this.name = name;
+        this.pos = pos;
+    }
+
+    public void run(){
+        System.out.println(name + " try eat!");
+        table.tryToEat(pos);
+        System.out.println(name + " got forks!");
+        table.eat(pos);
+        System.out.println(name + " has eaten! slurp");
     }
 }
