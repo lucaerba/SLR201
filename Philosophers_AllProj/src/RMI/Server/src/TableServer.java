80a4c112156753ff28c0892b284bf000d8972911
Luca Erbí <lucaerbi@air-di-luca.home>
2023-11-11 19:45:22 +0100

    finished

--- Philosophers_AllProj/src/RMI/Server/src/TableServer.java
+++ Philosophers_AllProj/src/RMI/Server/src/TableServer.java
@@ -1,44 +1,39 @@
 package RMI.Server.src;
 
 import static java.lang.Thread.sleep;
-import java.util.*;
-
+import java.util.Arrays;
+import java.util.Random;
 import RMI.TableI;
+
 public class TableServer implements TableI {
 
     private int nplaces = 5;
-    //private boolean[] forks = new boolean[5];
-    private List<Boolean> forks = Collections.synchronizedList(new ArrayList<>(5));
-
+    private boolean[] forks = new boolean[5];
     Random random = new Random();
 
     public TableServer(){
-        for (int i = 0; i < 5; i++) {
-            forks.add(true); // Initialize forks as available
-        }
+        Arrays.fill(forks, true);
     }
 
-    public boolean tryToEat(int pos) {
-        if (!forks.get(pos) || !forks.get((pos + 1) % nplaces)) {
-            System.out.println("Philosopher " + pos + " failed to get forks, go back thinking...");
+    public synchronized boolean tryToEat(int pos){
+        if(!forks[pos] || !forks[(pos+1)%nplaces]) {
+            System.out.println("Philosophe "+pos+" failed to get forks, go back thinking...");
             return false;
         }
-        forks.set(pos, false);
-        forks.set((pos + 1) % nplaces, false);
-        System.out.println("Philosopher " + pos + " got forks!!");
+        forks[pos] = forks[(pos+1)%nplaces] = false;
+        System.out.println("Philosophe "+pos+" got forks!!");
         return true;
     }
 
-    public void eat(int pos) {
+    public void eat(int pos){
         try {
-            System.out.println("Philosopher " + pos + " is eating!");
-            Thread.sleep(random.nextInt(256 + 1));
+            System.out.println("Philosophe "+pos+" is eating!");
+            sleep(random.nextInt(256+1));
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
-        } finally {
-            forks.set(pos, true);
-            forks.set((pos + 1) % nplaces, true);
-            System.out.println("Philosopher " + pos + " finished eating!...go back thinking");
         }
+        forks[pos] = forks[(pos+1)%nplaces] = true;
+        System.out.println("Philosophe "+pos+" finished eating!...go back thinking");
+        
     }
 }
